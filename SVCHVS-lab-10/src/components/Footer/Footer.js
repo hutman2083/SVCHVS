d015bc371395cec0f1c932f99cad5f9aaf3befd9
hutman2083 <mr.2003sasha@mail.ru>
2023-04-04 11:31:25 +0300

    Lab10
    feat: added lab 10

---
+++ SVCHVS-lab-10/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <footer>
+             <a href="https://github.com/hutman2083/SVCHVS">GitHub</a>
+        </footer>
+    )
+}
+
+export default Footer;
